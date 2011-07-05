2562c5e41a6cd561b83b5b3f91de9699791a5381
Ivan Dubrov <wfragg@gmail.com>
2011-07-05 17:41:08 +0700

    Implemented test for INVITE client transaction
    - test for timeout case implemented
    - now it is possible to run only few tx tests

--- lib/sip/test/sip_transaction_test.erl
+++ lib/sip/test/sip_transaction_test.erl
@@ -7,6 +7,9 @@
 %%%----------------------------------------------------------------
 -module(sip_transaction_test).
 
+%% Exports
+-compile(export_all).
+
 %% Include files
 -include_lib("sip_message.hrl").
 -include_lib("sip_transport.hrl").
@@ -24,36 +27,59 @@
 
 -spec transaction_test_() -> term().
 transaction_test_() ->
+    Tests = [client_invite_ok, 
+             client_invite_err, 
+             client_invite_timeout],
+    specs(Tests).
+
+%% @doc
+%% Generates EUnit specification for given tests. Each test is given
+%% as function name (atom).
+%% Could be used like:
+%% eunit:test(sip_transaction_test:test_([client_invite_ok]))
+%% @end
+-spec specs([atom()]) -> term().
+specs(Tests) ->
+    Transports = [tcp, udp],
+    TestFuns = lists:map(fun (Test) -> fun(T) -> sip_transaction_test:Test(T) end end, Tests),
+    {foreach, fun setup/0, fun cleanup/1, sip_test:with_timeout(for_transports(TestFuns, Transports), 60)}.
+
+setup() ->
     % Listen on 15060
     Cfg = sip_config:from_options([{udp, [15060]}, {tcp, [15060]}, {router, undefined}]),
-    Setup =
-        fun () -> {ok, Pid} = sip_transaction_sup:start_link(Cfg),
-                  % Mock transport layer calls
-                  meck:new(sip_transport, [passthrough]),
-                  SendRequest = fun (Conn, To, Msg) ->
-                                         ?MODULE ! {tp, Conn, {request, To, Msg}},
-                                         {ok, Conn}
-                                end,
-                  SendResponse = fun (Conn, Msg) ->
-                                          ?MODULE ! {tp, Conn, {response, Msg}},
-                                          {ok, Conn}
-                                 end,
-                  meck:expect(sip_transport, send_request, SendRequest),
-                  meck:expect(sip_transport, send_response, SendResponse),
-                  {Pid}
+    
+    {ok, Pid} = sip_transaction_sup:start_link(Cfg),
+    % Mock transport layer calls
+    meck:new(sip_transport, [passthrough]),
+    SendRequest = fun (Conn, To, Msg) ->
+                           ?MODULE ! {tp, Conn, {request, To, Msg}},
+                           {ok, Conn}
+                  end,
+    SendResponse = fun (Conn, Msg) ->
+                            ?MODULE ! {tp, Conn, {response, Msg}},
+                            {ok, Conn}
+                   end,
+    meck:expect(sip_transport, send_request, SendRequest),
+    meck:expect(sip_transport, send_response, SendResponse),
+    {Pid}.
+
+cleanup({Pid}) ->
+    meck:unload(sip_transport),
+    sip_test:shutdown_sup(Pid),
+    ok.
+
+%% Generate list of test functions with single parameter (resource created by setup)
+%% for every possible combination of test function and transport. Each test function
+%% from the list accepts single transport parameter.
+for_transports(Tests, Transports) ->
+    Fun =
+        fun (Transport) ->
+                 ApplyTest = fun(Test) ->
+                                     fun(_Res) -> Test(Transport) end
+                             end,
+                 lists:map(ApplyTest, Tests)
         end,
-    Cleanup =
-        fun ({Pid}) ->
-                 meck:unload(sip_transport),
-                 sip_test:shutdown_sup(Pid),
-                 ok
-        end,
-    {foreach, Setup, Cleanup,
-     sip_test:with_timeout([fun(_Res) -> client_invite_ok(udp) end,
-                            fun(_Res) -> client_invite_ok(tcp) end,
-                            fun(_Res) -> client_invite_err(udp) end,
-                            fun(_Res) -> client_invite_err(tcp) end], 60)
-    }.
+    lists:flatmap(Fun, Transports).
 
 %% Scenario tested:
 %% - client INVITE transaction is created
@@ -92,6 +118,9 @@ client_invite_ok(Transport) ->
     unregister(?MODULE),
     ok.
 
+%% Scenario tested:
+%% - client INVITE transaction is created
+%% - 5xx response is received
 client_invite_err(Transport)->
     register(?MODULE, self()),
 
@@ -127,11 +156,33 @@ client_invite_err(Transport)->
     ?assertReceive("Expect ACK to be sent by tx layer",
                    {tp, _Conn, {request, Remote, ACK}}),
 
-    % wait 32 seconds for unreliable transport only 
+    % wait 32 seconds for unreliable transport only
     sip_transport:is_reliable(Transport) orelse timer:sleep(32000),
 
     ?assertReceive("Expect tx to terminate after receiving final response",
                    {tx, TxRef, {terminated, normal}}),
+
+    unregister(?MODULE),
+    ok.
+
+%% Scenario tested:
+%% - client INVITE transaction is created
+%% - nothing happens in 32 seconds
+%% - transaction terminates due to the timeout
+client_invite_timeout(Transport)->
+    register(?MODULE, self()),
+
+    Remote = sip_test:endpoint(Transport),
+    Request = sip_test:invite(Transport),
+
+    {ok, TxRef} = sip_transaction:start_tx(client, self(), Remote, Request),
+    ?assertReceive("Expect first request to be sent by tx layer",
+                   {tp, _Conn, {request, Remote, Request}}),
+
+    timer:sleep(32000),
+
+    ?assertReceive("Expect tx to terminate after timeout",
+                   {tx, TxRef, {terminated, timeout}}),
 
     unregister(?MODULE),
     ok.
